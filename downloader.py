333f04eb43f975cbbae51a8c07b0f6dce623e9f4
Chetan <78348378+xLr864@users.noreply.github.com>
2025-04-10 13:50:03 +0530

    Added port and bindings

--- downloader.py
+++ downloader.py
@@ -311,4 +311,5 @@ def index():
     return render_template_string(HTML, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
