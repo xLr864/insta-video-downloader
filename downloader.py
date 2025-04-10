fe2c422e9078c022bdf62497c78ce2df99eeb51f
Chetan <78348378+xLr864@users.noreply.github.com>
2025-04-10 14:21:43 +0530

    Changed port from 10000 to 5000

--- downloader.py
+++ downloader.py
@@ -311,5 +311,5 @@ def index():
     return render_template_string(HTML, error=error)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
